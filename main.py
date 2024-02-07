863e3125a322f9edb0f5bb0159b641b30d2ebff8
Ducky <tymumm122@gmail.com>
2024-02-07 17:11:55 -0600

    Thunderbird 0.2

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import os
 from tkinter import *
-
+from tkinter import filedialog as fd
 directory = r"C:\Program Files (x86)\Steam\steamapps\common\Titanfall2\NorthstarLauncher.exe"  # Update with the correct application path
 
-# Function to change directory
+
+
 def change_directory():
     global directory
-    directory = entry.get()
-    entry.delete(0, END)
-    entry.insert(0, "Directory changed successfully!")
+    filetypes = (
+        ('executable files', '*.exe'),
+        ('All files', '*.*')
+    )
+    filename = fd.askopenfilename(
+        title='Select Northstar Launcher',
+        initialdir='/',
+        filetypes=filetypes
+    )
+    if filename: # if a file is selected
+        directory = filename # update the directory
+        entry.delete(0, END) # clear the entry box
+        entry.insert(0, "Directory changed successfully!") # show a message
 
 # Function to start Northstar
 def start_northstar():
     try:
         print(directory)
-        os.startfile(f"'{directory}'")
+        os.startfile(directory)
     except:
         show_error()
 
@@ -23,14 +34,14 @@ def show_error():
     global directory
     lbl1 = Label(root, text="Failed to Start Northstar!")
     lbl1.pack(side=BOTTOM, anchor=S)
-    change_button.pack(side=BOTTOM, anchor=S)
 
 # Function to start Northstar after a delay
 def start():
     root.after(0, start_northstar)
 
 root = Tk()
-root.title("Thunderbird 0.1")
+root.title("Thunderbird 0.2")
+root.state('zoomed')
 root.geometry("1920x1080")
 
 lbl = Label(root, text="Thunderbird is a Northstar Launcher and soon to be a Mod manager.")
@@ -42,6 +53,7 @@ btn.pack(side=BOTTOM, anchor=S)
 entry = Entry(root)
 entry.pack_forget()  # Remove the entry box
 
-change_button = Button(root, text="Change Directory", command=change_directory)
+change_button = Button(root, text="Locate Northstar", fg="Red", command=change_directory)
+change_button.pack(side=TOP, anchor=E)
 
 root.mainloop()
